a9aba104e3330d5d34a5b06c703d10624ad3ec27
missdfc <durekechika@gmail.com>
2024-04-26 18:37:40 +0100

    test 2

--- ecomm/models.py
+++ ecomm/models.py
@@ -43,6 +43,15 @@ STATE_CHOICES = (
 )
 
 # our users size choice
+STATUS_CHOICES = (
+    ('PENDING', 'Pending'),
+    ('PROCESSING', 'Processing'),
+    ('SHIPPED', 'Shipped'),
+    ('DELIVERED', 'Delivered'),
+    ('CANCELLED', 'Cancelled'),
+)
+
+# status of orders placed by our users
 SIZE_CHOICES = (
     ('S', 'Small'),
     ('M', 'Medium'),
@@ -51,6 +60,14 @@ SIZE_CHOICES = (
     ('XXL','Extra Extra large'),
 )
 
+# users rating of products
+RATING = (
+    (1, '★☆☆☆☆'),
+    (2, '★★☆☆☆'),
+    (3, '★★★☆☆'),
+    (4, '★★★★☆'),
+    (5, '★★★★★'),
+)
 
 # category of clothes i.e men,women, kids..
 class Category(models.Model):
@@ -78,7 +95,7 @@ class Customer(models.model):
 class Product(models.Model):
     product_name = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.ForeignKey
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     size = models.CharField(choices=SIZE_CHOICES, max_length=10)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=9999999999.99)
     product_image = models.ImageField(upload_to='uploads/product/')
@@ -99,43 +116,40 @@ class Cart(models.Model):
 # users wishlist
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey
-
-    def __str__(self):
-        return self.
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     
 # customers payment model
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    amount = models.
-    razor_order_id = models.
-    razor_payment_status = models
-    razor_payment_idn = models.
+    amount = models.DecimalField()
+    razor_order_id = models.CharField(max_length=100, blank=True, null=True)
+    razor_payment_status = models.CharField(max_length=100, blank=True, null=True)
+    razor_payment_idn = models.CharField(max_length=100, blank=True, null=True)
     paid = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.
 
 # orders placed by customers
 class OrderPlaced(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.
-    date_ordered = models.
-    status = models.
-    payment = models.
+    quantity = models.PositiveIntegerField(default=1)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, default='')
 
     def __str__(self):
-        return self.
+        return self.quantity * self.product.price
     
 # users review on our products
 class ProductReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     review = models.TextField()
-    rating = models.ImageField()
-    date = models.
+    rating = models.IntegerField(choices=RATING, default=None)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.
+        return self.product.product_name
+    
+    def get_rating(self):
+        return self.rating
