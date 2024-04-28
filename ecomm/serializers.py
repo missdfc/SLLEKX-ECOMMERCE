cfbf960ee00e1b9bc14ef1366e42a7de82fbb3ab
missdfc <durekechika@gmail.com>
2024-04-28 19:01:56 +0100

    3rd commit

--- ecomm/serializers.py
+++ ecomm/serializers.py
@@ -6,3 +6,46 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+# category serializer
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+# category serializer
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+# category serializer
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+# category serializer
+class WishlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = '__all__'
+
+# category serializer
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__' # come back to this
+
+# category serializer
+class OrderPlacedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderPlaced
+        fields = '__all__' # comee back to this
+
+# category serializer
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__' # come back to this
--- ecomm/views.py
+++ ecomm/views.py
@@ -11,3 +11,30 @@ class ListCategory(generics.ListAPIView):
 class DetailCategory(generics.RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer 
+
+# cproduct list views
+class ListProduct(generics.ListAPIView):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        search_term = self.request.query_params.get('search')
+
+        # print('Search term:', search_term)
+        if search_term:
+            queryset = Product.objects.filter(name__icontains=search_term)
+            print('filtered queryset:', queryset)
+            return queryset
+        else:
+            return Product.objects.all()
+
+# product list by category views
+class ListProduct(generics.ListAPIView):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self)
+        category_id = self.kwargs['category_id']
+
+# product detail views
+class DetailCategory(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer 
